0fc58170aa5175a210dd80719d7e1971136205cf
cwoerman <cwoerman@macalester.edu>
2020-10-20 09:18:22 -0500

    Re-submitted w/ shiny app correction

--- Shiny App/app.R
+++ Shiny App/app.R
@@ -4,30 +4,14 @@ library(rsconnect)
 ui <- fluidPage(
   selectInput("state",
               "State",
-              choices = list(Alabama = "AL",Alaska = "AK",Arizona = "AZ",
-                             Arkansas = "AR",California = "CA",Colorado = "CO",
-                             Connecticut = "CT", Deleware = "DE",Florida = "FL",
-                             Georgia = "GA", Hawaii = "HI", Idaho = "ID",
-                             Illinois = "IL", Indiana = "IN", Iowa = "IA",
-                             Kansas = "KN", Kentucky = "KY", Louisiana = "LA",
-                             Maine = "ME", Maryland = "MD", Massachusetts = "MA",
-                             Michigan = "MI", Minnesota = "MN", Mississippi = "MS",
-                             Missouri = "MO", Montana = "MT",Nebraska = "NE",
-                             Nevada = "NV",New_Hampshire= "NH", New_Jersey = "NJ",
-                             New_Mexico = "NM", New_York = "NY", North_Carolina = "NC",
-                             North_Dakota = "ND",Ohio = "OH",Oklahoma = "OK",
-                             Oregon = "OR", Pennsylvania = "PA", Rhode_Island = "RI",
-                             South_Carolina = "SC", South_Dakota = "SD", Tennessee = "TN",
-                             Texas = "TX", Utah = "UT", Vermont = "VT", Virginia = "VA",
-                             Washington = "WA", West_Virginia = "WV", Wisconsin = "WI",
-                             Wyoming = "WY")),
+              choices = unique(covid19$state)),
   submitButton(text = "Create my plot!"),
   plotOutput(outputId = "timeplot")
 )
 server <- function(input, output) {
-  output$timeplot <- renderPlot({
+  output$covidplot <- renderPlot({
     covid_weekly %>% 
-      filter(state == c(input$state)) %>% 
+      filter(state %in% c(input$state)) %>% 
     ggplot()
     aes(x = cases,
                y = new_cases_weekly,
